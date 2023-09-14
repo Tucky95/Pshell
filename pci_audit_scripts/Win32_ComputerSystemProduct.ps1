37a03a860d6b9b0514d42726958518663a0210a8
Tucky95 <jamest95@live.co.uk>
2023-09-14 15:54:49 +0100

    add pci folder

---
+++ pci_audit_scripts/Win32_ComputerSystemProduct.ps1
@@ -0,0 +1,28 @@
+﻿
+### You need to CD to c:\pstools before running this script ###
+
+
+### Sets the error action preference. "Continue" will display the error but continue running through list. "SilentlyContinue" to supress errors ###
+$ErrorActionPreference = "Continue"
+
+
+### Change csv/txt document with list of servers you need to run on ###
+
+    $servers = get-content "C:\SCRIPTS\CSV_s\next_csv\payment server names.txt"
+
+    
+    foreach ($server in $servers) {
+
+    ### Will show progress of script in green as it runs ###
+    
+        Write-host "Running psexec on $server" -ForegroundColor Green
+
+        
+    ### This script needs to be in the local machines c:\installs to run ###
+        
+        
+        Get-WmiObject -ComputerName $server -Class Win32_ComputerSystemProduct |Select-Object Pscomputername,Name,Vendor,Version | Out-File c:\Installs\PCI_Audit_files\Win32_ComputerSystemProduct_list.txt -Append
+
+}
+
+#Get-WmiObject -ComputerName $server -Class Win32_ComputerSystemProduct |Select-Object Pscomputername, Name,Vendor,Version
---
+++ pci_audit_scripts/compile wmic qfe docs into 1.ps1
@@ -0,0 +1,33 @@
+﻿
+# Path to csv with list of server names
+$Servers = import-csv "C:\SCRIPTS\CSV_s\next_csv\new 9.csv"
+
+
+
+$Servers | ForEach-Object { 
+
+    # Variables
+    $name = $_.Name
+    
+    # $file will be the name of the compiled list
+    $file = "wmic_qfe_list.txt"
+    
+    # $path 
+    $path = "\\" + $name + "\c$\installs\PCI_Audit_files\" + $file
+    $outfile = "C:\temp\compiled_wmic_qfe_list2.txt"
+    #$Target_path = $name2
+
+    #$get = Get-Content $path 
+    
+        # Will show progress
+        Write-Host Compiling info from $name 
+
+
+                #Runs the command to compile into 1 doc
+                 Get-ChildItem -Path $path -Filter "*.txt" | Get-Content | Add-Content -Path $outfile
+
+
+
+}
+
+
